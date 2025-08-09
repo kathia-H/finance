8886c1c287515e2f688504dd4168aad0950d9c73
KATHIA Hammami <hammamikathia@gmail.com>
2025-08-09 18:16:56 -0400

    contenu login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
-export class Login {
+export class LoginComponent {
+  email = '';
+  password = '';
+  message = '';
 
+  constructor(private auth: AuthService) {}
+
+  onSubmit() {
+    this.auth.login(this.email, this.password).subscribe({
+      next: (res) => {
+        // Stocke le token pour les futures requêtes
+        localStorage.setItem('jwtToken', res.token);
+        this.message = `Bienvenue, ${res.user.name || res.user.email}!`;
+      },
+      error: (err) => {
+        this.message = 'Échec de la connexion';
+        console.error(err);
+      }
+    });
+  }
 }
