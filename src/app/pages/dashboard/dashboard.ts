464319477996248007b364c75d933db7ec3db199
KATHIA Hammami <hammamikathia@gmail.com>
2025-08-09 18:29:01 -0400

    contenue dashbord

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrls: ['./dashboard.scss']
 })
-export class Dashboard {
-
+export class DashboardComponent {
 }
